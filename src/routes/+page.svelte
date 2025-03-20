c1b850baf7faab4ee5f6e93df75326b03826f159
Florian Bergmann <flobergmann@aol.ch>
2025-03-20 10:08:25 +0100

    add dynamic cube

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,26 @@
     import { onMount } from "svelte";
     import * as THREE from "three";
 
-    $: outerWidth = 0;
+    $: innerHeight = 0;
     $: innerWidth = 0;
     $: outerHeight = 0;
-    $: innerHeight = 0;
+    $: outerWidth = 0;
 
-    let container: Element;
-    const walkSpeed = 0.2;
-    const sprintSpeed = 0.5;
-    let moveSpeed = 0.1;
     const move = new THREE.Vector3(0, 0, 0);
     const keyPressed: string[] = [];
+    const sprintSpeed = 0.5;
+    const walkSpeed = 0.2;
+    const base = {
+        camera: {
+            x: 15,
+            y: 10,
+            z: 0,
+        },
+    };
+
+    let container: Element;
+    let moveSpeed = 0.1;
+    let scale = 1;
 
     onMount(() => {
         const scene = new THREE.Scene();
@@ -33,8 +42,7 @@
             1,
             1000,
         );
-        camera.position.x = 5;
-        camera.position.y = 10;
+        camera.position.set(base.camera.x, base.camera.y, base.camera.z);
 
         // Floor
         const floorGeometry = new THREE.PlaneGeometry(50, 50); // Width and height of the plane
@@ -43,102 +51,26 @@
             side: THREE.DoubleSide,
         });
         const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-        floor.position.y = 0;
+        floor.position.y = -0.01;
         floor.rotation.x = Math.PI / 2;
         floor.receiveShadow = true;
         scene.add(floor);
 
-        // Stickman
-        const head = new THREE.Bone();
-        const neck = new THREE.Bone();
-        const shoulder = new THREE.Bone();
-        const rightArm = new THREE.Bone();
-        const leftArm = new THREE.Bone();
-        const rightLeg = new THREE.Bone();
-        const leftLeg = new THREE.Bone();
-
-        head.add(neck);
-        neck.add(shoulder);
-        neck.add(rightArm);
-        neck.add(leftArm);
-        shoulder.add(rightLeg);
-        shoulder.add(leftLeg);
-
-        const bones = [
-            head,
-            neck,
-            shoulder,
-            rightArm,
-            leftArm,
-            rightLeg,
-            leftLeg,
-        ];
-
-        head.position.y = 5;
-        head.position.z = -2;
-
-        neck.position.y = -0.3;
-
-        shoulder.position.y = -1.5;
-
-        rightArm.position.y = -1;
-        rightArm.position.z = 1;
-
-        leftArm.position.y = -1;
-        leftArm.position.z = -1;
-
-        rightLeg.position.y = -2;
-        rightLeg.position.z = 1;
-
-        leftLeg.position.y = -2;
-        leftLeg.position.z = -1;
-
-        const geometry = new THREE.CylinderGeometry(0.2, 0.2);
-        const position = geometry.attributes.position;
-
-        const vertex = new THREE.Vector3();
-
-        const skinIndices = [];
-        const skinWeights = [];
-
-        for (let i = 0; i < position.count; i++) {
-            vertex.fromBufferAttribute(position, i);
-
-            // compute skinIndex and skinWeight based on some configuration data
-            const y = vertex.y + 1;
-            const skinIndex = Math.floor(y / 2);
-            const skinWeight = (y % 1) / 1;
-            skinIndices.push(skinIndex, skinIndex + 1, 0, 0);
-            skinWeights.push(1 - skinWeight, skinWeight, 0, 0);
-        }
-
-        geometry.setAttribute(
-            "skinIndex",
-            new THREE.Uint16BufferAttribute(skinIndices, 4),
-        );
-        geometry.setAttribute(
-            "skinWeight",
-            new THREE.Float32BufferAttribute(skinWeights, 4),
-        );
-
-        // create skinned mesh and skeleton
         // Cube
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const mesh = new THREE.SkinnedMesh(geometry, material);
-        const skeleton = new THREE.Skeleton(bones);
+        const geometry = new THREE.BoxGeometry(2, 2, 2);
+        const material = new THREE.MeshBasicMaterial({ color: 0x176288 });
+        const cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 1;
+        cube.position.z = 1;
+        scene.add(cube);
 
-        // see example from THREE.Skeleton
-        mesh.add(head);
-
-        // bind the skeleton to the mesh
-        mesh.bind(skeleton);
-
-        scene.add(mesh);
-
-        const helper = new THREE.SkeletonHelper(mesh);
-        scene.add(helper);
-
-        camera.lookAt(head.position);
+        const edges = new THREE.EdgesGeometry(geometry);
+        const lines = new THREE.LineSegments(
+            edges,
+            new THREE.LineBasicMaterial({ color: 0xffffff }),
+        );
+        cube.add(lines);
+        camera.lookAt(cube.position);
 
         // Light
         const light1 = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -149,6 +81,14 @@
         // Helpers
         const axesHelper = new THREE.AxesHelper(5);
         scene.add(axesHelper);
+
+        const arrowHelper = new THREE.ArrowHelper(
+            new THREE.Vector3(1, 0, 0),
+            new THREE.Vector3(0, 0, 0),
+            2,
+            0x00000,
+        );
+        cube.add(arrowHelper);
 
         // Keyboard control
         function isOneOfKeysPressed(keys: string[]) {
@@ -176,30 +116,42 @@
             moveSpeed = event.shiftKey ? sprintSpeed : walkSpeed;
         }
 
-        function onKeyDown(event: KeyboardEvent) {
-            checkShiftKey(event);
-            if (keyPressed.indexOf(event.code) === -1) {
-                keyPressed.push(event.code);
-                updateMove();
-            }
-        }
+        document.addEventListener(
+            "keydown",
+            (event) => {
+                checkShiftKey(event);
+                if (keyPressed.indexOf(event.code) === -1) {
+                    keyPressed.push(event.code);
+                    updateMove();
+                }
+            },
+            false,
+        );
 
-        function onKeyUp(event: KeyboardEvent) {
-            checkShiftKey(event);
-            const index = keyPressed.indexOf(event.code);
-            if (index >= 0) {
-                delete keyPressed[index];
-                updateMove();
-            }
-        }
+        document.addEventListener(
+            "keyup",
+            (event) => {
+                checkShiftKey(event);
+                const index = keyPressed.indexOf(event.code);
+                if (index >= 0) {
+                    delete keyPressed[index];
+                    updateMove();
+                }
+            },
+            false,
+        );
 
-        document.addEventListener("keydown", onKeyDown, false);
-        document.addEventListener("keyup", onKeyUp, false);
+        addEventListener("wheel", (event) => {
+            scale = Math.min(Math.max(0.125, scale + event.deltaY * -0.001), 4);
+        });
 
         function animate() {
-            camera.position.x = (head.position.x += move.x) + 5;
-            camera.position.z = head.position.z += move.z;
-            camera.lookAt(head.position);
+            camera.position.x =
+                (cube.position.x += move.x) + base.camera.x / scale;
+            camera.position.z = cube.position.z += move.z;
+
+            cube.rotation.y = -new THREE.Vector2(move.x, move.z).angle();
+            camera.lookAt(cube.position);
 
             renderer.render(scene, camera);
         }
