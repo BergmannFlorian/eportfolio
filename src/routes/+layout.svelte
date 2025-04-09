55c249dec43e54da1b015f7ea98c59bbc57c62a1
Florian Bergmann <flobergmann@aol.ch>
2025-04-09 09:07:55 +0200

    argg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
-    const { children } = $props();
     import "../app.css";
+    import { base } from "$app/paths";
+
+    const { children } = $props();
 </script>
+
+<nav>
+    <a href="{base}/">home</a>
+    <a href="{base}/3d">3D</a>
+    <a href="{base}/pdf">PDF</a>
+</nav>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,6 @@
     let cv = $state(data.cv ? (data.cv as CV) : null);
 </script>
 
-<nav>
-    <a href="/">home</a>
-    <a href="/3d">3D</a>
-    <a href="/pdf">PDF</a>
-</nav>
-
 {#if cv}
     <h1>{cv.infos.contact.name}</h1>
 {/if}
