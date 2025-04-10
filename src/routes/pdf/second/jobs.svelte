e6394aff1953a87715d4541066a6a7c64184bfd0
Florian Bergmann <flobergmann@aol.ch>
2025-04-10 15:49:15 +0200

    fix file name error

--- src/routes/pdf/second/jobs.svelte
+++ src/routes/pdf/second/jobs.svelte
@@ -3,12 +3,6 @@
     import Job from "./Job.svelte";
 
     export let jobs: JobI[];
-
-    function displayProjects(projects: Project[]): boolean {
-        if (projects.length < 1) return false;
-        if (projects.length === 1 && projects[0].link === "") return false;
-        return true;
-    }
 </script>
 
 <div class="w-full flex flex-col py-7">
